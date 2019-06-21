d5aea9b30c765e667f155fd9608e0f39f0884b90
轻剑快马 <xiaoranran1993@outlook.com>
2019-06-21 18:10:51 +0800

    refactor: 重构一部分代码

---
+++ test/index.test.js
@@ -0,0 +1,5 @@
+import {} from '../lib/index'
+
+describe('测试 index.js 的函数', () => {
+  test('', () => {})
+})
--- util.spec.js
+++
@@ -1,5 +0,0 @@
-import {} from './util'
-
-describe('测试 Utils 的函数', () => {
-  test('', () => {})
-})
