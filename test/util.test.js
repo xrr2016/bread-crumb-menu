2dd2eadca07c137f01f8a245120da84cb1ab8142
轻剑快马 <xiaoranran1993@outlook.com>
2019-06-25 11:40:42 +0800

    test: 添加验证和测试代码

--- test/util.test.js
+++ test/util.test.js
@@ -1,31 +1,64 @@
-const { isValidFolderName } = require('../lib/util')
+const { isValidFileName, isValidChineseName } = require('../lib/util')
 
-describe('测试 util.isValidFolderName 的函数', () => {
+describe('测试 util.isValidFileName 的函数', () => {
   test('是一个函数', () => {
-    expect(typeof isValidFolderName).toBe('function')
+    expect(typeof isValidFileName).toBe('function')
   })
 
   test('不传参数时返回 false', () => {
-    expect(isValidFolderName()).toBe(false)
+    expect(isValidFileName()).toBe(false)
   })
 
   test('传无效参数时返回 false', () => {
-    expect(isValidFolderName(null)).toBe(false)
-    expect(isValidFolderName(undefined)).toBe(false)
-    expect(isValidFolderName('')).toBe(false)
-    expect(isValidFolderName({})).toBe(false)
-    expect(isValidFolderName(1234)).toBe(false)
-    expect(isValidFolderName(' ')).toBe(false)
+    expect(isValidFileName(null)).toBe(false)
+    expect(isValidFileName(undefined)).toBe(false)
+    expect(isValidFileName('')).toBe(false)
+    expect(isValidFileName({})).toBe(false)
+    expect(isValidFileName([])).toBe(false)
+    expect(isValidFileName(' ')).toBe(false)
   })
 
   test('传无效字符串参数时返回 false', () => {
-    expect(isValidFolderName('-')).toBe(false)
-    expect(isValidFolderName('<')).toBe(false)
-    expect(isValidFolderName('/bbb')).toBe(false)
+    expect(isValidFileName('-')).toBe(false)
+    expect(isValidFileName('<')).toBe(false)
+    expect(isValidFileName('/bbb')).toBe(false)
   })
 
   test('传有效字符串参数时返回 true', () => {
-    expect(isValidFolderName('aaa')).toBe(true)
-    expect(isValidFolderName('aaa-bbb')).toBe(true)
+    expect(isValidFileName('aaa')).toBe(true)
+    expect(isValidFileName('aaa-bbb')).toBe(true)
+  })
+})
+
+describe('测试 util.isValidChineseName 的函数', () => {
+  test('是一个函数', () => {
+    expect(typeof isValidChineseName).toBe('function')
+  })
+
+  test('不传参数时返回 false', () => {
+    expect(isValidChineseName()).toBe(false)
+  })
+
+  test('传无效参数时返回 false', () => {
+    expect(isValidChineseName(null)).toBe(false)
+    expect(isValidChineseName(undefined)).toBe(false)
+    expect(isValidChineseName('')).toBe(false)
+    expect(isValidChineseName({})).toBe(false)
+    expect(isValidChineseName([])).toBe(false)
+    expect(isValidChineseName(' ')).toBe(false)
+  })
+
+  test('传无效字符串参数时返回 false', () => {
+    expect(isValidChineseName('-')).toBe(false)
+    expect(isValidChineseName('<')).toBe(false)
+    expect(isValidChineseName('/bbb')).toBe(false)
+    expect(isValidChineseName('aaa')).toBe(false)
+    expect(isValidChineseName('aaa-bbb')).toBe(false)
+  })
+
+  test('传有效字符串参数时返回 true', () => {
+    expect(isValidChineseName('啊啊啊')).toBe(true)
+    expect(isValidChineseName('笨笨笨')).toBe(true)
+    expect(isValidChineseName('惨惨惨')).toBe(true)
   })
 })
